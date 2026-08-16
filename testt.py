85ec9599d0c975642435161f86806ee633e8989a
jbt <amitaibroder07@gmail.com>
2026-08-16 15:20:14 +0300

    rawad

--- testt.py
+++ testt.py
@@ -1,10 +1,9 @@
 import pygame
 import consts
+import random
 def init_game():
     pygame.init()
-    screen = pygame.display.set_mode(
-        (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT)
-    )
+    screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
     pygame.display.set_caption("The Flag")
     clock = pygame.time.Clock()
     return screen, clock
@@ -17,7 +16,9 @@ def Loading_assets():
     flag_resized = pygame.transform.scale(
         flag_image, (consts.FLAG_WIDTH, consts.FLAG_HEIGHT)
     )
-    return soldier_resized, flag_resized
+    grass_image = pygame.image.load("grass.png")
+    grass_resized = pygame.transform.scale(grass_image, (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
+    return soldier_resized, flag_resized, grass_resized
 def Incident_Handling():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,31 +35,35 @@ def update_player_position(x, y):
     if keys[pygame.K_DOWN]:
         y += 5
     return x, y
-def draw_screen(screen, soldier, flag, player_x, player_y, flag_x, flag_y):
+def draw_screen(screen, soldier, flag, grass, grass_positions, player_x, player_y, flag_x, flag_y):
     screen.fill(consts.DARK_GREEN)
+    for pos in grass_positions:
+        screen.blit(grass, pos)
     screen.blit(flag, (flag_x, flag_y))
     screen.blit(soldier, (player_x, player_y))
     pygame.display.flip()
 def main():
     screen, clock = init_game()
-    soldier_resized, flag_resized = Loading_assets()
+    soldier_resized, flag_resized, grass_resized = Loading_assets()
+
     player_x, player_y = 0, 0
     flag_x = consts.WINDOW_WIDTH - consts.FLAG_WIDTH
     flag_y = consts.WINDOW_HEIGHT - consts.FLAG_HEIGHT
+    grass_positions = []
+    for i in range(20):
+        rand_x = random.randint(0, consts.WINDOW_WIDTH - consts.GRASS_WIDTH)
+        rand_y = random.randint(0, consts.WINDOW_HEIGHT - consts.GRASS_HEIGHT)
+        grass_positions.append((rand_x, rand_y))
     running = True
     while running:
         clock.tick(60)
         running = Incident_Handling()
         player_x, player_y = update_player_position(player_x, player_y)
         draw_screen(
-            screen,
-            soldier_resized,
-            flag_resized,
-            player_x,
-            player_y,
-            flag_x,
-            flag_y,
+            screen, soldier_resized, flag_resized, grass_resized, grass_positions, player_x, player_y, flag_x, flag_y
         )
     pygame.quit()
 if __name__ == "__main__":
     main()
+
+
